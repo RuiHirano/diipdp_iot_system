a62869846f7ecd5846d3119e51f6f234aa426fac
RuiHirano <r.hrn.0930@gmail.com>
2020-09-09 17:05:05 +0900

    add pybluez_beacon_scan_sample.py

--- src/pybluez_beacon_scan_sample.py
+++ src/pybluez_beacon_scan_sample.py
@@ -1,7 +1,27 @@
-from bluetooth.ble import DiscoveryService
+from bluetooth.ble import BeaconService
 
-service = DiscoveryService()
-devices = service.discover(2)
+class Beacon(object):
+    
+    def __init__(self, data, address):
+        self._uuid = data[0]
+        self._major = data[1]
+        self._minor = data[2]
+        self._power = data[3]
+        self._rssi = data[4]
+        self._address = address
+        
+    def __str__(self):
+        ret = "Beacon: address:{ADDR} uuid:{UUID} major:{MAJOR}"\
+                " minor:{MINOR} txpower:{POWER} rssi:{RSSI}"\
+                .format(ADDR=self._address, UUID=self._uuid, MAJOR=self._major,
+                        MINOR=self._minor, POWER=self._power, RSSI=self._rssi)
+        return ret
 
-for address, name in devices.items():
-    print("name: {}, address: {}".format(name, address))
+service = BeaconService()
+devices = service.scan(2)
+
+for address, data in list(devices.items()):
+    b = Beacon(data, address)
+    print(b)
+
+print("Done.")
