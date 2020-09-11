ac05ec1aba987d9fe0948e8d86a70e2cfc7aa488
RuiHirano <r.hrn.0930@gmail.com>
2020-09-12 00:56:24 +0900

    update sample

--- src/bluepy_notify.py
+++ src/bluepy_notify.py
@@ -3,6 +3,7 @@ import binascii
 import sys
 
 HANDLE_ACC = 0x001d
+NOTIFY = 0x0028
 
 exflag = False
 
@@ -32,7 +33,7 @@ def main():
 
     # ボタン notify を要求
     peri.writeCharacteristic(HANDLE_ACC, b'\x01\x00', True)
-    peri.writeCharacteristic(0x0028, "\x01\x00", True) # 通知有効化
+    peri.writeCharacteristic(NOTIFY, "\x01\x00", True) # 通知有効化
     #peri.writeCharacteristic(0x002a, b"\x50\x00", True) # 80ms ごとに通知 #デフォルトは 20ms
     #peri.writeCharacteristic(0x0028, b"\x01\x00", True) # 通知有効化
 
