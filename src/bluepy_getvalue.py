520678038dd7a056b58e59eca2c8a1b15f64a94d
RuiHirano <r.hrn.0930@gmail.com>
2020-09-12 16:55:37 +0900

    update sample

--- src/bluepy_getvalue.py
+++ src/bluepy_getvalue.py
@@ -9,9 +9,9 @@ SERVICE_DATA2 = 0x0016  # HardwareVersion
 SERVICE_DATA3 = 0x0018  # FirmwareVersion
 SERVICE_DATA4 = 0x001a # Software Version
 SERVICE_DATA5 = 0x001d # ??? b'F\xd9c\x0e\xad\x14u@\xcaM\xbd/\x10\xeb\x066', 2: b'\xb2l\xf70D\xbd\xc3(}\x98\x19\xca\x11\xfa1\xba'
-SERVICE_DATA6 = 0x0021 
-SERVICE_DATA7 = 0x0023
-SERVICE_DATA8 = 0x0025
+SERVICE_DATA6 = 0x0021 # none
+SERVICE_DATA7 = 0x0023 # none
+SERVICE_DATA8 = 0x0025 # none
 SERVICE_DATA9 = 0x0027
 SERVICE_DATA10 = 0x0029
 SERVICE_DATA11 = 0x002b
@@ -41,8 +41,8 @@ def main():
     #print( "Service Data7: %s" % sdata )
     #sdata = peri.readCharacteristic(SERVICE_DATA8)
     #print( "Service Data8: %s" % sdata )
-    sdata = peri.readCharacteristic(SERVICE_DATA9)
-    print( "Service Data9: %s" % sdata )
+    #sdata = peri.readCharacteristic(SERVICE_DATA9)
+    #print( "Service Data9: %s" % sdata )
     sdata = peri.readCharacteristic(SERVICE_DATA10)
     print( "Service Data10: %s" % sdata )
     sdata = peri.readCharacteristic(SERVICE_DATA11)
