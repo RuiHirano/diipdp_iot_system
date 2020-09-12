53fe28e444c583bc3b4ee266ffdc9f8a7cfd310b
RuiHirano <r.hrn.0930@gmail.com>
2020-09-12 16:48:36 +0900

    update sample

--- src/bluepy_getvalue.py
+++ src/bluepy_getvalue.py
@@ -2,18 +2,51 @@ import sys
 import bluepy
 
 HANDLE_DEVNAME = 0x0009
-HANDLE_SERIAL = 0x000c
-SERVICE_DATA = 0x0010
+HANDLE_SERIAL = 0x0010
+SERVICE_DATA = 0x0012
+SERVICE_DATA1 = 0x0014
+SERVICE_DATA2 = 0x0016
+SERVICE_DATA3 = 0x0018
+SERVICE_DATA4 = 0x001a
+SERVICE_DATA5 = 0x001d
+SERVICE_DATA6 = 0x0021
+SERVICE_DATA7 = 0x0023
+SERVICE_DATA8 = 0x0025
+SERVICE_DATA9 = 0x0027
+SERVICE_DATA10 = 0x0029
+SERVICE_DATA11 = 0x002b
 
 def main():
     peri = bluepy.btle.Peripheral()
     peri.connect(devadr, bluepy.btle.ADDR_TYPE_PUBLIC)
     devname = peri.readCharacteristic(HANDLE_DEVNAME)
-    print( "Device Name: %s" % devname.decode("utf-8") )
+    print( "Device Name: %s" % devname )
     serialnum = peri.readCharacteristic(HANDLE_SERIAL)
     print( "Serial Number: %s" % serialnum )
     sdata = peri.readCharacteristic(SERVICE_DATA)
     print( "Service Data: %s" % sdata )
+    sdata = peri.readCharacteristic(SERVICE_DATA1)
+    print( "Service Data1: %s" % sdata )
+    sdata = peri.readCharacteristic(SERVICE_DATA2)
+    print( "Service Data2: %s" % sdata )
+    sdata = peri.readCharacteristic(SERVICE_DATA3)
+    print( "Service Data3: %s" % sdata )
+    sdata = peri.readCharacteristic(SERVICE_DATA4)
+    print( "Service Data4: %s" % sdata )
+    sdata = peri.readCharacteristic(SERVICE_DATA5)
+    print( "Service Data5: %s" % sdata )
+    sdata = peri.readCharacteristic(SERVICE_DATA6)
+    print( "Service Data6: %s" % sdata )
+    sdata = peri.readCharacteristic(SERVICE_DATA7)
+    print( "Service Data7: %s" % sdata )
+    sdata = peri.readCharacteristic(SERVICE_DATA8)
+    print( "Service Data8: %s" % sdata )
+    sdata = peri.readCharacteristic(SERVICE_DATA9)
+    print( "Service Data9: %s" % sdata )
+    sdata = peri.readCharacteristic(SERVICE_DATA10)
+    print( "Service Data10: %s" % sdata )
+    sdata = peri.readCharacteristic(SERVICE_DATA11)
+    print( "Service Data11: %s" % sdata )
     peri.disconnect()
 
 if __name__ == "__main__":
